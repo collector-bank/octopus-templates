a835f45a39b15d292130afe2219c6963568f6e6c
Per Jahn <perjahn@gmail.com>
2018-12-27 14:14:08 +0000

    2nd ver

--- deployrobust.ps1
+++ deployrobust.ps1
@@ -1,42 +1,50 @@
+#$ResourceGroup = "test"
+#$WebAppName = "test"
+#$SlotName = "test"
+#$Domain = "test"
+
 function WaitForExistanceOfSlot
 {
-	Write-Host "WaitForExistanceOfSlot"
+    Write-Host "WaitForExistanceOfSlot"
     $retryCount = 0
     while(!(Get-AzureRMWebAppSlot -ResourceGroupName $ResourceGroup -Name $WebAppName -Slot $SlotName -ErrorAction SilentlyContinue ))
     {
-      Write-Host "Failed to get staging slot. Retry count=$retryCount"
-      Clear-DnsClientCache
-      Sleep -Seconds 5
-      $retryCount++
-      if ($retryCount -eq 50) {
-        exit 1
-      }
-    }    	
+        Write-Host "Failed to get staging slot. Retry count=$retryCount"
+        Clear-DnsClientCache
+        Sleep -Seconds 5
+        $retryCount++
+        if ($retryCount -eq 50) {
+            exit 1
+        }
+    }
 }
 
-function WaitForRemovalOfSlot
+function WaitForRemovalOfSlot([int] $removeRetries)
 {
-    Write-Host "WaitForRemovalOfSlot $Domain"
+    Write-Host "WaitForRemovalOfSlot $Domain, Retry count=$removeRetries"
     $retryCount = 0
     while(Get-AzureRMWebAppSlot -ResourceGroupName $ResourceGroup -Name $WebAppName -Slot $SlotName -ErrorAction SilentlyContinue )
     {
-    	Write-Host "Staging slot still not removed. Retry count=$retryCount"
-    	Sleep -Seconds 5
-    	$retryCount++
-    	if ($retryCount -eq 50) {
-    		exit 1
-    	}
-    }    	
+        Write-Host "Staging slot still not removed. Retry count=$retryCount"
+        Sleep -Seconds 5
+        $retryCount++
+        if ($retryCount -eq 50) {
+            return $false
+        }
+    }
+    return $true
 }
 
 Write-Host "New-DeploymentSlot: ResourceGroup=$ResourceGroup, WebAppName = $WebAppName, SlotName=staging"
 
-# Remove the staging slot if it exists
-$oldSlot = Get-AzureRMWebAppSlot -ResourceGroupName $ResourceGroup -Name $WebAppName -Slot $SlotName -ErrorAction SilentlyContinue
-Remove-AzureRMWebAppSlot -Name $WebAppName -Slot $SlotName -ResourceGroupName $ResourceGroup -Force -ErrorAction SilentlyContinue
-
-# Wait for slot to be removed
-WaitForRemovalOfSlot
+[int] $removeRetries=0
+do
+{
+    # Suggest to azure that the staging slot might be removed
+    $oldSlot = Get-AzureRMWebAppSlot -ResourceGroupName $ResourceGroup -Name $WebAppName -Slot $SlotName -ErrorAction SilentlyContinue
+    Remove-AzureRMWebAppSlot -Name $WebAppName -Slot $SlotName -ResourceGroupName $ResourceGroup -Force -ErrorAction SilentlyContinue
+}
+while (!(WaitForRemovalOfSlot ($removeRetries++)) -and $removeRetries -lt 10)
 
 # Create the api staging slot
 $newSlot = New-AzureRMWebAppSlot -Name $WebAppName -Slot $SlotName -ResourceGroupName $ResourceGroup -ErrorAction Stop
@@ -47,7 +55,7 @@ WaitForExistanceOfSlot
 # Stop web jobs in staging slot
 $webAppSlot = Get-AzureRMWebAppSlot -ResourceGroupName $ResourceGroup -Name $WebAppName -Slot $SlotName
 $appSettingsClone = @{}
-foreach ($kvp in  $webAppSlot.SiteConfig.AppSettings)
+foreach ($kvp in $webAppSlot.SiteConfig.AppSettings)
 {
     $appSettingsClone[$kvp.Name] = $kvp.Value
 }
@@ -57,12 +65,12 @@ Set-AzureRmWebAppSlot -Name $WebAppName -Slot $SlotName -ResourceGroupName $Reso
 #Write-Host "Web job appsettings applied: `n $($appSettingsClone | ConvertTo-Json)"
 
 $stickySlotConfigObject = @{
-	"connectionStringNames" = @()
-	"appSettingNames" =  @("WEBJOBS_STOPPED")
+    "connectionStringNames" = @()
+    "appSettingNames" = @("WEBJOBS_STOPPED")
 }
 
 ## Sets the "WEBJOBS_STOPPED" setting as a sticky setting on all the slots
-Set-AzureRmResource -PropertyObject $stickySlotConfigObject -ResourceGroupName $ResourceGroup -ResourceType Microsoft.Web/sites/config -ResourceName  $WebAppName/slotConfigNames -ApiVersion 2015-08-01 -Force | Out-Null
+Set-AzureRmResource -PropertyObject $stickySlotConfigObject -ResourceGroupName $ResourceGroup -ResourceType Microsoft.Web/sites/config -ResourceName $WebAppName/slotConfigNames -ApiVersion 2015-08-01 -Force | Out-Null
 
 # Log finish
 Write-Host "New-DeploymentSlot done."
